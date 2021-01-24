35d9668fc54253c3ac9a83fc675c96893b33a68a
Antoine <antoinecldn@gmail.com>
2021-01-24 16:56:17 +0100

    Initial Comit

---
+++ public/js/script.js
@@ -0,0 +1,183 @@
+$('document').ready(function(){
+  //PAGE ACCUEIL
+    if($('body').hasClass('homepage')){
+        $('.footer-page').hide();
+
+      // Partie scroll
+        $('section').not('.current').hide();
+        var progressBarH = $('.progress-bar').height();
+        var progressVal = 9.09090909091;
+        var progressBarFinal;
+
+        var scrollEnabled = true;
+
+
+        function disableScroll() {
+            // temporarily disable action
+            scrollEnabled = false;
+
+            // set a timer to enable again it 1 second from now
+            setTimeout(function() {
+                scrollEnabled = true;
+            }, 1000);
+        }
+
+        $('body').on('mousewheel', function(event) {
+            $('section').not('.current').hide();
+            var delta = event.deltaY;
+            var data = $('.current').data('scroll');
+
+ 
+          //up
+            if(delta > 0){
+              if(data !== 0 && scrollEnabled){
+                  data = data-1;
+                  $('section').removeClass('current');
+                  $('section[data-scroll='+data+']').addClass('current');
+                  $('section').not('.current').hide();
+                  $('.current').fadeIn(1200);
+                  progressBarFinal =  (progressBarH-progressVal);
+                  progressBarH = progressBarFinal;
+                  $('.progress-bar').height(progressBarFinal+'%');
+                  disableScroll();
+              }
+          //Down
+            }else{
+
+              if(data !== 11 && scrollEnabled){
+
+         
+                  data = data+1;
+                  $('section').removeClass('current');
+                  $('section[data-scroll='+data+']').addClass('current');
+                  $('section').not('.current').hide();
+                  $('.current').fadeIn(1200);
+                  progressBarFinal =  (progressBarH+progressVal);
+                  progressBarH = progressBarFinal;
+                  $('.progress-bar').height(progressBarFinal+'%');
+                  disableScroll();
+                
+
+              }
+
+            }
+            if( data == 0 || data == 11){
+              $('footer').fadeIn();
+            }else{
+     
+              $('footer').hide();
+              
+          
+            }
+            switch (data) {
+              case 0:
+                $( "#menu-left li" ).css('color','white');
+                break;
+              case 1:
+              case 2:
+              case 3:
+              case 4:
+              case 5:
+              case 6:
+                $( "#menu-left li a" ).css('color','white');
+                $( "#graphisme" ).css('color','#1E90FF');
+                break;
+              case 7:
+              case 8:
+              case 9:
+                $( "#menu-left li a" ).css('color','white');
+                $( "#photographie").css('color','#1E90FF');
+                break;
+              case 10:
+                $( "#menu-left li a" ).css('color','white');
+                $( "#boutique" ).css('color','#1E90FF');
+                break;
+              case 11:
+                $( "#menu-left li a" ).css('color','white');
+                break;
+            }
+        });
+      //AFFICHAGE AU CLIC SUR MENU ACCUEIL
+      $( "#menu-web a" ).click(function() {
+        var idMenu = $(this).attr('id');
+        switch(idMenu) {
+          case 'graphisme':
+            $('section').removeClass('current');
+            $('section[data-scroll=1]').addClass('current');
+            $('section').not('.current').hide();
+            $('.current').fadeIn(1200);
+            progressBarFinal = progressVal;
+            progressBarH = progressBarFinal;
+            $('.progress-bar').height(progressBarFinal+'%');
+            $( "#menu-left li a" ).css('color','white');
+            $( "#graphisme" ).css('color','#1E90FF');
+            $('.footer-home').hide();
+            disableScroll();
+
+            break;
+          case 'photographie':
+            $('section').removeClass('current');
+            $('section[data-scroll=7]').addClass('current');
+            $('section').not('.current').hide();
+            $('.current').fadeIn(1200);
+            progressBarFinal =  (progressVal*7);
+            progressBarH = progressBarFinal;
+            $('.progress-bar').height(progressBarFinal+'%');
+            $( "#menu-left li a" ).css('color','white');
+            $( "#photographie" ).css('color','#1E90FF');
+            $('.footer-home').hide();
+            disableScroll();
+            break;     
+          case 'boutique':
+            $('section').removeClass('current');
+            $('section[data-scroll=10]').addClass('current');
+            $('section').not('.current').hide();
+            $('.current').fadeIn(1200);
+            progressBarFinal =  (progressVal*10);
+            progressBarH = progressBarFinal;
+            $('.progress-bar').height(progressBarFinal+'%');
+            $( "#menu-left li a" ).css('color','white');
+            $( "#boutique" ).css('color','#1E90FF');
+            $('.footer-home').hide();
+            disableScroll();
+            break;    
+
+        }
+
+      });
+    }else{
+        $('.footer-home').hide();
+    }
+
+
+    // AUTRE PAGES
+
+    //HOVER SUR LES IMG
+    $('.gallery-img').hover(function() {
+      $(this).find('img').css('opacity', '0.3');
+      $(this).find('.img-title').fadeIn(300);
+      $(this).find('.img-desc').fadeIn(300);
+    }, function() {
+      $(this).find('img').css('opacity', '1');
+      $(this).find('.img-title').fadeOut(100);
+      $(this).find('.img-desc').fadeOut(100);
+    });
+
+    //MODAL SUR LES IMG
+    $( ".gallery-img img" ).click(function() {
+      var src = $(this).attr("src");
+      
+      if($(this).attr('id')){
+        $('.modal-photo').css('width', '1380px');
+      }
+      
+      $(this).css('visibility', 'hidden');
+      $('.modal-img').attr('src',src);
+      $('.modal-photo').attr('src',src);
+      $('#imgModal').modal('show');
+    });
+    
+    $('#imgModal').on('hidden.bs.modal', function (e) {
+      $( ".gallery-img img" ).css('visibility','visible');
+    })
+});
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('accueil.html.twig');
+    }
+
+    /**
+     * @Route("/pochettes-officielles", name="offi")
+     */
+    public function offiAction(): Response
+    {
+        return $this->render('Pages/officielles.html.twig');
+    }
+
+    /**
+     * @Route("/pochettes-alternatives", name="alter")
+     */
+    public function alterAction(): Response
+    {
+        return $this->render('Pages/alternatives.html.twig');
+    }
+
+    /**
+     * @Route("/enter-in-my-vision", name="vision")
+     */
+    public function visionAction(): Response
+    {
+        return $this->render('Pages/vision.html.twig');
+    }
+
+    /**
+     * @Route("/logos", name="logo")
+     */
+    public function logoAction(): Response
+    {
+        return $this->render('Pages/logos.html.twig');
+    }
+
+    /**
+     * @Route("/affiches-et-posters", name="affiche")
+     */
+    public function afficheAction(): Response
+    {
+        return $this->render('Pages/affiches.html.twig');
+    }
+
+    /**
+     * @Route("/wallpaper", name="wallpaper")
+     */
+    public function wallpaperAction(): Response
+    {
+        return $this->render('Pages/wallpaper.html.twig');
+    }
+
+    /**
+    * @Route("/concert-13block", name="concert_13block")
+    */
+    public function concert_13blockAction(): Response
+    {
+        return $this->render('Pages/concert_13block.html.twig');
+    }
+
+    /**
+     * @Route("/concert-zamdane", name="concert_zamdane")
+     */
+    public function concert_zamdaneAction(): Response
+    {
+        return $this->render('Pages/concert_zamdane.html.twig');
+    }
+
+    /**
+     * @Route("/concert-palace-party", name="concert_palace")
+     */
+    public function concert_palaceAction(): Response
+    {
+        return $this->render('Pages/concert_palace.html.twig');
+    }
+
+        /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(): Response
+    {
+        return $this->render('Pages/contact.html.twig');
+    }
+
+}
